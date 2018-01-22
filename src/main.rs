f2cc92e980ccf5f38406774e69fa601c79c82377
Weston Carlson <wcarlson@vendasta.com>
2018-01-22 16:56:45 -0600

    Filter on search terms dynamically.

--- src/main.rs
+++ src/main.rs
@@ -14,18 +14,25 @@ fn allbud(search_terms: Vec<String>) {
     let search_resp = reqwest::get(&search_url).unwrap();
     assert!(search_resp.status().is_success());
 
-    let strain_urls = Vec::new();
+    let mut strain_urls = Vec::new();
     Document::from_read(search_resp).unwrap().find(Class("object-title")).for_each(|node| {
         let a_tags = node.find(Name("a"));
         let search_strains = a_tags.map(|tag| format!("{}{}", base_url, tag.attr("href").unwrap()));
-        let filtered_strain_urls = search_strains.filter(|strain| strain.contains("jesus")).filter(|strain| strain.contains("og"));
-        filtered_strain_urls.for_each(|url| strain_urls.push(url)));
-    })
+        let filtered_strain_urls = search_strains.filter(|strain| {
+            let mut contains_terms = true;
+            search_terms.iter().for_each(|term| {
+                contains_terms &= strain.contains(term);
+            });
+            return contains_terms;
+        });
+        filtered_strain_urls.for_each(|url| strain_urls.push(url));
+    });
 
     for url in strain_urls {
-        let strain_resp = reqwest::get(&search_url).unwrap();
-        assert!(strain_resp.status().is_success());
-
-        Document::from_read(search_resp).unwrap().
+        println!("{}", url);
+//        let strain_resp = reqwest::get(&search_url).unwrap();
+//        assert!(strain_resp.status().is_success());
+//
+//        Document::from_read(search_resp).unwrap().
     }
 } 
