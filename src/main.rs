e310c9d95a7ae52b0e823b557a38bf47996ba6ca
Weston Carlson <wcarlson@vendasta.com>
2018-01-24 18:21:32 -0600

    Add initial leafly search scraper.

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,11 @@ extern crate select;
 extern crate reqwest;
 
 use select::document::Document;
-use select::predicate::{Attr, Class, Name};
+use select::predicate::{Attr, Class, Name, Predicate};
 
 fn main() {
-    allbud(vec!["jesus".to_string(), "og".to_string()])
+    //allbud(vec!["jesus".to_string(), "og".to_string()]);
+    leafly(vec!["jesus".to_string(), "og".to_string()]);
 }
 
 fn allbud(search_terms: Vec<String>) {
@@ -39,4 +40,40 @@ fn allbud(search_terms: Vec<String>) {
         let name = split_url.last().unwrap().replace("-", " ");
         println!("{}:\trating: {}\tnum_ratings: {}", name, rating.inner_html().trim(), num_ratings.inner_html().trim());
     }
-} 
+}
+
+fn leafly(search_terms: Vec<String>) {
+    let base_url = "https://www.leafly.com";
+    let search_url = format!("{}{}{}{}", base_url, "/search?q=", search_terms.join("+"), "&typefilter=strain");
+    let search_resp = reqwest::get(&search_url).unwrap();
+    assert!(search_resp.status().is_success());
+
+    let doc = Document::from_read(search_resp).unwrap();
+    doc.find(Name("li").descendant(Class("padding-rowItem"))).for_each(|item| {
+        println!("{} {}", item.inner_html(), item.text());
+        println!("******\n******");
+    })
+
+//    let a_tags = node.find(Name("a"));
+//    let search_strains = a_tags.map(|tag| format!("{}{}", base_url, tag.attr("href").unwrap()));
+//    let filtered_strain_urls = search_strains.filter(|strain| {
+//        let mut contains_terms = true;
+//        search_terms.iter().for_each(|term| {
+//            contains_terms &= strain.contains(term);
+//        });
+//        return contains_terms;
+//    });
+//    filtered_strain_urls.for_each(|url| strain_urls.push(url));
+//
+//    for url in strain_urls {
+//        let strain_resp = reqwest::get(&url).unwrap();
+//        assert!(strain_resp.status().is_success());
+//
+//        let doc = Document::from_read(strain_resp).unwrap();
+//        let rating = doc.find(Class("rating-num")).next().unwrap();
+//        let num_ratings = doc.find(Attr("id", "product-rating-votes")).next().unwrap();
+//        let split_url: Vec<&str> = url.split('/').collect();
+//        let name = split_url.last().unwrap().replace("-", " ");
+//        println!("{}:\trating: {}\tnum_ratings: {}", name, rating.inner_html().trim(), num_ratings.inner_html().trim());
+//    }
+}
