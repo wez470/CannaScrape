d92267388aae03605502f58d7dd21251fa6b394a
Weston <wez470@gmail.com>
2018-01-25 18:58:53 -0600

    Finish leafly scraper.

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,9 @@ use select::document::Document;
 use select::predicate::{Attr, Class, Name, Predicate};
 
 fn main() {
-    //allbud(vec!["jesus".to_string(), "og".to_string()]);
+    println!("ALLBUD\n----------");
+    allbud(vec!["jesus".to_string(), "og".to_string()]);
+    println!("\nLEAFLY\n----------");
     leafly(vec!["jesus".to_string(), "og".to_string()]);
 }
 
@@ -50,46 +52,29 @@ fn leafly(search_terms: Vec<String>) {
 
     let doc = Document::from_read(search_resp).unwrap();
     let mut names = Vec::new();
-//    let mut num_reviews = Vec::new();
-//    let mut ratings = Vec::new();
+    let mut num_reviews = Vec::new();
+    let mut ratings = Vec::new();
     doc.find(Name("li").descendant(Class("padding-rowItem")).descendant(Class("copy--bold"))).for_each(|item| {
         let name = item.text().trim().to_lowercase();
-        let mut contains_terms = true;
-        search_terms.iter().for_each(|term| {
-            contains_terms &= name.contains(term);
-        });
-        if contains_terms {
-            names.push(name);
-        }
+        names.push(name);
     });
     doc.find(Name("li").descendant(Class("padding-rowItem")).descendant(Class("color--light"))).for_each(|item| {
-        let raw_num_revs = item.text().trim().to_string();
-        let num_revs = raw_num_revs.split_whitespace().next().unwrap().to_string().split_off(1);
-
-        println!("{}", num_revs.to_string());
-        //num_reviews.push(num_revs);
+        let match_chars: &[_] = &['(', ')', ' '];
+        let num_revs = item.text().trim_matches(match_chars).split_whitespace().next().unwrap().to_string();
+        num_reviews.push(num_revs);
+    });
+    doc.find(Name("li").descendant(Class("padding-rowItem")).descendant(Name("img"))).for_each(|item| {
+        let rating = item.attr("src").unwrap().split('/').nth(2).unwrap();
+        ratings.push(rating);
     });
 
-//    let a_tags = node.find(Name("a"));
-//    let search_strains = a_tags.map(|tag| format!("{}{}", base_url, tag.attr("href").unwrap()));
-//    let filtered_strain_urls = search_strains.filter(|strain| {
-//        let mut contains_terms = true;
-//        search_terms.iter().for_each(|term| {
-//            contains_terms &= strain.contains(term);
-//        });
-//        return contains_terms;
-//    });
-//    filtered_strain_urls.for_each(|url| strain_urls.push(url));
-//
-//    for url in strain_urls {
-//        let strain_resp = reqwest::get(&url).unwrap();
-//        assert!(strain_resp.status().is_success());
-//
-//        let doc = Document::from_read(strain_resp).unwrap();
-//        let rating = doc.find(Class("rating-num")).next().unwrap();
-//        let num_ratings = doc.find(Attr("id", "product-rating-votes")).next().unwrap();
-//        let split_url: Vec<&str> = url.split('/').collect();
-//        let name = split_url.last().unwrap().replace("-", " ");
-//        println!("{}:\trating: {}\tnum_ratings: {}", name, rating.inner_html().trim(), num_ratings.inner_html().trim());
-//    }
+    for review in names.iter().zip(num_reviews.iter().zip(ratings.iter())) {
+        let mut contains_terms = true;
+        search_terms.iter().for_each(|term| {
+            contains_terms &= review.0.contains(term);
+        });
+        if contains_terms {
+            println!("{}:\trating: {}\tnum_ratings: {}", review.0, (review.1).1, (review.1).0);
+        }
+    }
 }
